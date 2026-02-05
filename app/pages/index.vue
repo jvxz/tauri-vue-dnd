fb14e051f721c385bbfd1ceb73b54319f91d5e33
jamie <hi@jamie.to>
2026-02-05 16:14:12 -0500

    chore: add docs toc omponent

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+const DRAG_UPDATE_THRESHOLD = 10
 const list = shallowRef([1, 2, 3, 4, 5, 6, 7])
 
 // whether the user is dragging an item
@@ -8,17 +9,29 @@ const isDragging = shallowRef(false)
 const draggingItem = shallowRef<number | null>(null)
 
 const pointer = usePointer()
-const { pressed: isMouseDown } = useMousePressed()
+const { pressed: isMouseDown } = useMousePressed({
+  onReleased: () => {
+    // reset the dragging state when the mouse button is released
+    isDragging.value = false
+    draggingItem.value = null
+  },
+})
 
-const DRAG_UPDATE_THRESHOLD = 10
+// counter for the number of times the pointer has been updated
 let dragUpdateCount = 0
+// item that is potentially being dragged
 let potentialDraggingItem: number | null = null
 const { pause: pausePointerWatch, resume: resumePointerWatch } = watch([pointer.x, pointer.y], () => {
+  // pause the watcher if the the user is not pressing the mouse button
+  // or if the item is already being dragged
   if (!isMouseDown.value || isDragging.value)
     return pausePointerWatch()
 
+  // increment the counter when the pointer is updated (moved)
   dragUpdateCount++
 
+  // if the pointer has been updated (moved) enough times,
+  // declare that the item is being dragged
   if (dragUpdateCount >= DRAG_UPDATE_THRESHOLD) {
     dragUpdateCount = 0
     isDragging.value = true
@@ -29,13 +42,9 @@ const { pause: pausePointerWatch, resume: resumePointerWatch } = watch([pointer.
   immediate: false,
 })
 
-whenever(() => !isMouseDown.value, () => {
-  pausePointerWatch()
-  isDragging.value = false
-  draggingItem.value = null
-})
-
 function handleDragStart(item: number) {
+  // set mousedown manually because the watcher thinks 
+  // the mouse is not down
   isMouseDown.value = true
   resumePointerWatch()
   potentialDraggingItem = item
